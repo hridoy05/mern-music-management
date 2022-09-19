56baaf15529591fe1aab3db3e181fe029cacf136
Hridoy Banik <hridoy.banik71@gmail.com>
2022-09-20 01:16:35 +0600

    auth

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,52 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { SetUser } from "../redux/userSlice";
+import { HideLoading, ShowLoading } from "../redux/alertsSlice";
+
+
+function ProtectedRoute({ children }) {
+ 
+  const { user } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const [readyToRender, setReadyToRender] = useState(false);
+  const dispatch = useDispatch();
+  const getUserData = async () => {
+    try {
+      dispatch(ShowLoading())
+      const response = await axios.post(
+        "/api/users/get-user-data",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(HideLoading())
+      if (response.data.success) {
+        dispatch(SetUser(response.data.data));
+      } else {
+        alert(response.data.message);
+      }
+      setReadyToRender(true);
+    } catch (error) {
+      dispatch(HideLoading())
+      localStorage.removeItem("token");
+      setReadyToRender(true);
+      navigate("/login");
+    }
+  };
+
+  useEffect(() => {
+    if (user === null) {
+      getUserData();
+    }
+  }, []);
+
+  return <div>{readyToRender && children}</div>;
+}
+
+export default ProtectedRoute;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div className="flex gap-5">
+        <div className="w-1/2">
+          SongsList
+        </div>
+        <div className="w-1/2">
+          Playlists
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
