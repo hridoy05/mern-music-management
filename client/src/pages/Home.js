ecbc65d3085a3cc0a381380017ddc106b1ea5e09
Hridoy Banik <hridoy.banik71@gmail.com>
2022-09-22 21:12:32 +0600

    songList component added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,20 @@
 import React from "react";
+import SongsList from "../components/SongsList";
+//import Playlists from "../components/Playlists";
+//import Player from "../components/Player";
 
 function Home() {
   return (
     <>
       <div className="flex gap-5">
         <div className="w-1/2">
-          SongsList
+          <SongsList />
         </div>
         <div className="w-1/2">
-          Playlists
+          {/* <Playlists /> */}
         </div>
       </div>
+      {/* <Player /> */}
     </>
   );
 }
